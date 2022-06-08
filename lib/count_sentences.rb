bd40ab10e018bcdd31cbf51683d9df121ca88b07
Serje <Serje.havandjian@gmail.com>
2022-06-08 10:59:25 -0700

    commit tree 8f91c7be88723606bea339953a2fbf97bb8e17f5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else 
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
-  end
+    num = 0
+      self.split(".").each do |word|
+          if !word
+            0
+          else
+          num +=1
+          end
+      end
+      num
+    end
 end
+
+
